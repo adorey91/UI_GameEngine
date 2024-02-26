554b7946127a8ba9ff62133c6c00497756ff3627
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2024-02-26 12:52:13 -0400

    Initial commit

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreAmount;
+
+    private int score = 0;
+   
+
+    void Start()
+    {
+        scoreAmount.text = "Score: " + score.ToString();
+    }
+
+    public void ScoreChange(int value)
+    {
+        if (value == 0)
+            score = 0;
+        else
+            score += value;
+
+        
+        if(score <= 0)
+            score = 0;
+        if (score > 999999999)
+            score = 999999999;
+
+        scoreAmount.text = "Score: " + score.ToString("#,##0");
+    }
+}
